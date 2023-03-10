dc8b9f5b098a4cdc5214c862c0c975a59332d6ba
天马行空 <1124099628@qq.com>
2023-03-10 19:57:38 +0800

    0310第三次

--- src/main/java/com/practice/juc/ThreadPoolDemo.java
+++ src/main/java/com/practice/juc/ThreadPoolDemo.java
@@ -1,7 +1,6 @@
 package com.practice.juc;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 class Bank{
     private static int num = 50;
@@ -19,17 +18,40 @@ public class ThreadPoolDemo {
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
         Bank bank = new Bank();
-        for (int i = 1; i <= 50; i++) {
+        custom(bank);
+/*        for (int i = 1; i <= 50; i++) {
             cachedThreadPool.submit(()->{
                 bank.deal();
             },String.valueOf(i));
-        }
-
-
+        }*/
 /*        for (int i = 1; i <= 50; i++) {
             fixedThreadPool.submit(()->{
                 bank.deal();
             },String.valueOf(i));
         }*/
     }
+    private static void custom(Bank bank) {
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                3L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                threadPool.submit(()->{
+                    bank.deal();
+                },String.valueOf(i));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+
 }
